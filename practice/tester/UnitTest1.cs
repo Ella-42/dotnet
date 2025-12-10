0f117bbe0cf949f3ec9c466cdd84a95a61e847c5
Ella <EllaP.jobs@gmail.com>
2025-12-10 19:27:21 +0100

    added some practice(auto push :heart:)

---
+++ practice/tester/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace tester;
+
+public class UnitTest1
+{
+    [Theory]
+	[InlineData(2,2,4)]
+	[InlineData(5,-1,4)]
+	[InlineData(9,4,13)]
+    public void Test1(int x, int y, int result)
+    {
+		Assert.Equal(result, x + y);
+    }
+}
